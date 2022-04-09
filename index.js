8eb1884f06b171cd520916889653e1dc721e5e96
Saqlain Majeed <saqlainmajeed811@gmail.com>
2022-04-09 16:25:55 +0500

    index file name changed

--- index.js
+++ index.js
@@ -1,122 +1,78 @@
-const form = document.querySelector('.form');
-const formGroupEls = document.querySelectorAll('.form-group');
-const firstNameEl = document.querySelector('.first_name');
-const lastNameEl = document.querySelector('.last_name');
-const emailEl = document.querySelector('.email');
-const passwordEl = document.querySelector('.password');
-const confirmPasswordEl = document.querySelector('.confirm_password');
-const submitbtn = document.querySelector('.btn--submit');
-const body = document.querySelector('body');
-const errorEl = document.querySelector('.error-msg');
-const headingEl = document.querySelector('.heading');
-const inputEls = document.querySelectorAll('input');
-const errText = document.querySelector('.err-text');
-
-submitbtn.addEventListener('click', (e) => {
+// submitting event on form
+const formEl = document.querySelectorAll('.form')[0];
+formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-  validate();
+  if (validate()) {
+    alert('Hurrah! Your form is submitted Successfully');
+    clearFields();
+  }
 });
 
-const validate = () => {
-  // ----------alert on each form-group--------
-  formGroupEls.forEach((element) => {
-    let id = element.id;
+// Setting error function
+const setError = (id, err) => {
+  document.querySelector(`#${id} .err`).innerText = err;
+};
 
-    if (id !== '') {
-      const errMsg = document.createElement('span');
-      errMsg.innerHTML = 'This field is required';
-      element.classList.add('error');
-      element.appendChild(errMsg);
+// Clearing field function After Submittion
+const clearFields = () => {
+  let inputsEl = Array.from(document.getElementsByTagName(`input`));
+
+  inputsEl.forEach((input) => {
+    if (input.value === 'submit') {
+      return;
     }
+    input.value = '';
   });
-
-  if (confirmPasswordEl) {
-    if (confirmPasswordEl.value === '' && passwordEl.value !== '') {
-      errText.innerHTML += 'please re-enter password';
-      return;
-    }
-    if (
-      confirmPasswordEl.value === passwordEl.value &&
-      confirmPasswordEl.value !== ''
-    ) {
-      console.log('password matched');
-      return;
-    } else if (
-      confirmPasswordEl.value !== passwordEl.value &&
-      confirmPasswordEl.value !== ''
-    ) {
-      console.log('password not matched');
-      return;
-    }
-  } else if (confirmPasswordEl.value === '') {
-    console.log('please confirm password');
-  }
-
-  inputEls.values = '';
 };
 
-const required = (input) => {
-  if (input.value === '') {
-    const msg = document.createElement('p');
-    msg.innerHTML = `${input.name} is required`;
-    msg.classList.add('error_msg');
-    errorEl.appendChild(msg);
+// clear Error function
+const clearErrors = () => {
+  let errors = document.getElementsByClassName('err');
 
-    setTimeout(() => {
-      msg.innerHTML = '';
-    }, 3000);
-  } else if (input.value !== '') {
-    return;
+  for (let item of errors) {
+    item.innerText = '';
   }
 };
+// validation function
+const validate = () => {
+  clearErrors();
+  let returnVal = true;
+  const ffnameEl = document.querySelector('.ffname').value;
+  const flnameEl = document.querySelector('.flname').value;
+  const femailEl = document.querySelector('.femail').value;
+  const fpasswordEl = document.querySelector('.fpassword').value;
+  const fcpasswordEl = document.querySelector('.fcpassword').value;
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-// let messages = [];
-// form.addEventListener('submit', (e) => {
-//   if (firstNameEl) {
-//     requiredCheck(firstNameEl);
-//   }
-
-//   if (lastNameEl) {
-//     requiredCheck(lastNameEl);
-//   }
-
-//   if (messages.length > 0) {
-//     e.preventDefault();
-//     errorEl.innerText = messages.join(', ');
-//   }
-// });
-
-// const requiredCheck = (input) => {
-//   if (messages.length > 0) {
-//     messages = [];
-//   }
-//   if (input.value === '' || input.value === null) {
-//     messages.push(`${input.name} is required`);
-//   }
-// };
-
-/*
-// ------------PREVIOUS FIELD CHECK------------
-// empty field checks
-
-  if (firstNameEl.value === '') {
-    // required(firstNameEl);
-    console.log(firstNameEl.parentElement);
-    errText.innerHTML = 'First Name is required <br>';
+  if (ffnameEl === '') {
+    setError('ffname', 'Please! enter first name');
+    returnVal = false;
+  }
+  if (flnameEl === '') {
+    setError('flname', 'Please! enter last name');
+    returnVal = false;
   }
 
-  if (lastNameEl.value === '') {
-    errText.innerHTML += 'Last Name is required <br>';
-    // required(lastNameEl);
+  if (femailEl === '') {
+    setError('femail', 'Please enter email');
+  } else if (!femailEl.match(mailformat)) {
+    setError('femail', 'Please enter valid email');
   }
 
-  if (emailEl.value === '') {
-    // required(emailEl);
-    errText.innerHTML += 'Email is required <br>';
+  if (fpasswordEl === '') {
+    setError('fpassword', 'Please! enter password');
+    returnVal = false;
+  } else if (fpasswordEl.includes('password')) {
+    setError('fpassword', `Password can't contain word "password"`);
+    returnVal = false;
+  }
+  if (fcpasswordEl === '') {
+    setError('fcpassword', 'Please! re-enter the password');
+    returnVal = false;
+  } else if (fcpasswordEl !== fpasswordEl) {
+    setError('fcpassword', `Oops! Password doesn't match`);
+    returnVal = false;
   }
 
-  if (passwordEl.value === '') {
-    errText.innerHTML += 'Password is required <br>';
-    // required(passwordEl);
-  }
-  */
+  return returnVal;
+};
